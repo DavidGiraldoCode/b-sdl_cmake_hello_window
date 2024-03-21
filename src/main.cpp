420fd0220759ab055c3177484c3efe9ce34e573f
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-03-21 18:20:35 +0100

    Organized methods in setup, draw and event

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+int status = 0;
 const int WIDTH = 1024;
 const int HEIGHT = 768;
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 
-int main(int argc, char* argv[])
+void setup()
 {
-    std::cout << "Hello Window! \n";
-
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
-        return -1;
+        status = -1;
     }
-    else
-    {
-        std::cout << "SDL_Init succeeded \n";
-    }
+    std::cout << "SDL_Init succeeded \n";
+}
 
-    SDL_Window* window = SDL_CreateWindow("Video Game - SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+void createWindowRenderer(const int WIDTH, const int HEIGHT, SDL_Window* & window, SDL_Renderer* & renderer )
+{
+    window = SDL_CreateWindow("Video Game - SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if (!window)
     {
         std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
         SDL_Quit();
-        return -1;
+        status = -1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer)
     {
         std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
-        return -1;
+        status = -1;
     }
+}
 
-    // Set the background color to black
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+void draw(SDL_Renderer* & renderer)
+{
+    SDL_RenderClear(renderer); //Clean the render before drawing something else
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); //Set the color RGBa of what you want to draw
+    SDL_RenderDrawPoint(renderer, WIDTH/2, HEIGHT/2); //Render a point in the middle of the screen
+    SDL_RenderPresent(renderer); //Show it in the window
+}
 
-    // Render a single white pixel in the middle
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDrawPoint(renderer, WIDTH / 2, HEIGHT / 2);
-    SDL_RenderPresent(renderer);
-
+void events()
+{
     bool isRunning = true;
     SDL_Event event;
     while (isRunning)
@@ -56,9 +59,23 @@ int main(int argc, char* argv[])
             }
         }
     }
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello Window! \n";
+    setup();
+    createWindowRenderer(WIDTH, HEIGHT, window, renderer);
+    if(status != 0)
+    {
+        return -1;
+    }
+    draw(renderer);
+    events();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
